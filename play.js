06c74b8e4ce04ffb4513007f0e6ccc510d02f07a
modemgirl <serra@modemgirl.ca>
2022-02-20 23:15:38 +0000

    add connect function

--- play.js
+++ play.js
@@ -1,18 +1,22 @@
 const net = require('net'); //node's net module
 
-const connect = () => {  
-//function that allows us to connect to the snek server
+const connect = () => {
+//when called, creates new connection to the server
+//use ctrl+c to terminate
 
   const conn = net.createConnection({
-    host: "localhost",  
-    port: 3000
+    host: "10.0.2.15",
+    port: 50541
   });
 
   conn.setEncoding('utf8');   //interpret incoming data as text
 
-  return conn;
+  conn.on('data', (data) => {
+    console.log(data);        //log incoming data to console
+  });
 
+  return conn;
 };
 
 console.log('connecting...');
-connect(); 
+connect();
