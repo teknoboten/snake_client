86c59ba14eb4620f9a49d439f614a721b52f846e
modemgirl <serra@modemgirl.ca>
2022-02-20 23:36:33 +0000

    moved connect function to client.js

---
+++ client.js
@@ -0,0 +1,21 @@
+const net = require('net'); //node's net module
+
+const connect = () => {
+//when called, creates new connection to the server
+//use ctrl+c to terminate
+
+  const conn = net.createConnection({
+    host: "10.0.2.15",
+    port: 50541
+  });
+
+  conn.setEncoding('utf8');   //interpret incoming data as text
+
+  conn.on('data', (data) => {
+    console.log(data);        //log incoming data to console
+  });
+
+  return conn;
+};
+
+module.exports = { connect };
--- play.js
+++ play.js
@@ -1,22 +1,5 @@
-const net = require('net'); //node's net module
+const { connect }  = require('./client');
 
-const connect = () => {
-//when called, creates new connection to the server
-//use ctrl+c to terminate
-
-  const conn = net.createConnection({
-    host: "10.0.2.15",
-    port: 50541
-  });
-
-  conn.setEncoding('utf8');   //interpret incoming data as text
-
-  conn.on('data', (data) => {
-    console.log(data);        //log incoming data to console
-  });
-
-  return conn;
-};
 
 console.log('connecting...');
 connect();
