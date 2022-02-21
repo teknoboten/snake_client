9cd1afc64d6b8a4cc7d302f56164bbb3dc0ee659
modemgirl <serra@modemgirl.ca>
2022-02-21 21:39:10 +0000

    refactor setupInput and handleUserInput to input.js

---
+++ input.js
@@ -0,0 +1,27 @@
+const handleUserInput = (data) => {
+  //callback to call when stdin recieves data
+  
+    if (data === '\u0003') {
+      process.exit();
+    };
+    process.stdout.write('u quit');   //how i make this work?
+  }
+  
+  
+  const setupInput = function () {
+  // setup interface to handle user input from stdin
+  
+    const stdin = process.stdin;    //stdin is an object
+    stdin.setRawMode(true);
+    stdin.setEncoding("utf8");
+    stdin.resume();
+  
+    stdin.on('data', (data) => {
+      handleUserInput(data);
+    });
+  
+    return stdin; 
+  };
+
+  
+  module.exports = { setupInput }; 
